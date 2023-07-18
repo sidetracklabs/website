c86fbd9f02ac600fdc05429418d35e102ac9bfef
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2023-07-18 12:32:55 -0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Docs
+
+Sidetrack labs docs based on Nextra
+
+## Quick Start
+
+Clone this repo.
+
+## Local Development
+
+First, run `pnpm i` to install the dependencies.
+
+Then, run `pnpm dev` to start the development server and visit localhost:3000.
---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Sidetrack</span>,
+  project: {
+    link: "https://github.com/sidetracklabs",
+  },
+  // chat: {
+  // link: 'https://discord.com',
+  // },
+  docsRepositoryBase: "https://github.com/sidetracklabs/website",
+  footer: {
+    text: "Sidetrack Labs",
+  },
+};
+
+export default config;
