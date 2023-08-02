750b1f133893abfca167313ab89d6837ab7eb9b6
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2023-08-02 13:04:52 -0700

    feat: add logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Image from "next/image";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import logo from "./assets/logo.jpg";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
-  logo: <span>Sidetrack</span>,
+  logo: <Image src={logo} alt={"Logo"} width={25} height={25} />,
   project: {
     link: "https://github.com/sidetracklabs",
   },
@@ -10,9 +13,55 @@ const config: DocsThemeConfig = {
   // link: 'https://discord.com',
   // },
   docsRepositoryBase: "https://github.com/sidetracklabs/website",
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s – Sidetrack",
+      };
+    }
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Sidetrack" />
+      <meta
+        property="og:description"
+        content="An open source, database-backed, typescript-first background job library."
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+    </>
+  ),
   footer: {
     text: "Sidetrack Labs",
   },
+  // footer: {
+  //   text: (
+  //     <span>
+  //       {/* MIT {new Date().getFullYear()} ©{" "} */}
+  //       {new Date().getFullYear()}{" "}
+  //       <a href="https://sidetrack.run" target="_blank">
+  //         Sidetrack Labs
+  //       </a>
+  //     </span>
+  //   ),
+  // },
 };
 
 export default config;
