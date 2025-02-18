8d22b4d69b83a579d17106f45a91c15c925d8ae9
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2025-02-17 22:39:22 -0500

    build: separate eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,8 +21,8 @@ export default [
     "next",
     "next/core-web-vitals",
     "plugin:mdx/recommended",
+    "prettier",
   ),
-  eslintPluginPrettierRecommended,
   {
     rules: {
       "react/no-unescaped-entities": "off",
