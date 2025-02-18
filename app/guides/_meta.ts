ccbb8eb47875a7fe9ec9cbd6ff4a30d4e0877f6a
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2025-02-17 22:27:40 -0500

    docs: add insert job guide

--- app/guides/_meta.ts
+++ app/guides/_meta.ts
@@ -1,10 +1,11 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   "configuring-queues": "Configuring Queues",
+  "inserting-jobs": "Inserting Jobs",
   "working-with-types": "Working With Types",
   "running-migrations": "Running Migrations",
   "custom-db-client": "Custom Database Clients",
-  "cron-jobs": "Cron Jobs",
+  "cron-jobs": "Recurring (Cron) Jobs",
   "testing-sidetrack": "Testing Sidetrack",
   "errors-and-logging": "Errors and Logging",
   "using-effect": "Using Sidetrack With Effect",
